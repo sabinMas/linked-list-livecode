faac9b1fcbd58bf922f05b5ec6a253281f522a3b
sabinMas <202682616+sabinMas@users.noreply.github.com>
2026-04-28 11:55:08 -0700

    livecode follow

--- src/Practice.java
+++ src/Practice.java
@@ -1,5 +1,24 @@
 public class Practice {
     public static void main(String[] args) {
-        System.out.println("~linked lists practice~");
+        Node myNode = new Node('w',null);
+        Node otherNode = new Node('x',null);
+        Node coolNode = new Node('e',null);
+
+        Node current = myNode;
+        while(current != null){
+            System.out.println(current.value);
+            current = current.next;
+        }
+
+
+
+        myNode.next = otherNode;
+        otherNode.next = coolNode;
+
+        System.out.println(myNode.value);
+        System.out.println(myNode.next.value);
+        System.out.println(myNode.next.next.value);
+
+
     }
 }
